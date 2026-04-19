934e3d8b9d4c253a931cd2b1190b8b64fa06118c
Rahull-06 <rahullrathod06@gmail.com>
2026-04-19 23:31:01 +0530

    Started Searching Technique

--- 03_arrays/searching/BinarySearch/FindPeakEle.java
+++ 03_arrays/searching/BinarySearch/FindPeakEle.java
@@ -1,3 +1,22 @@
 public class FindPeakEle {
-    
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 1};
+        System.out.println(findPeak(arr));
+    }
+    public static int findPeak(int[] arr) {
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if ((mid == 0 || arr[mid] >= arr[mid - 1]) && (mid == arr.length - 1 || arr[mid] >= arr[mid + 1])) {
+                return mid;
+            } else if (mid > 0 && arr[mid - 1] > arr[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1; // This line will never be reached since there is always a peak element
+    }
 }
--- 05_sorting/bubbleSort/OptimizedSort.java
+++ 05_sorting/bubbleSort/OptimizedSort.java
@@ -1,3 +1,4 @@
+
 public class OptimizedSort {
     public static void main(String[] args) {
         int[] arr = {5, 1, 4, 2, 8};
